912f2cce7e37664ab29bff186c84db8a1f155bd8
nthaong246 <030239230220@st.buh.edu.vn>
2026-04-07 10:03:46 +0700

    hienthichitiet

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\MovieController;
 
 Route::get('/', [MovieController::class, 'index']);
 use App\Http\Controllers\MovieController1;
-Route::get('/movie/chitiet/{id}', [MovieController1::class, 'chitiet'])->name('movie.chitiet');
+Route::get('/chitiet/{id}', [MovieController1::class, 'chitiet'])->name('movie.chitiet');
